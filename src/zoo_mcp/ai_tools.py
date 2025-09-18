a24294b5ef60ef79b86bad287d9eadb669e68ea6
Ryan Barton <ryan@zoo.dev>
2025-09-18 15:39:57 -0400

    add multi file iteration text to cad tool

--- src/zoo_mcp/ai_tools.py
+++ src/zoo_mcp/ai_tools.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from pathlib import Path
 
 import aiofiles
@@ -8,10 +9,12 @@ from kittycad.models import (
     FileExportFormat,
     TextToCadCreateBody,
     TextToCadIterationBody,
+    TextToCadMultiFileIterationBody,
 )
 from kittycad.models.text_to_cad_response import (
     OptionTextToCad,
     OptionTextToCadIteration,
+    OptionTextToCadMultiFileIteration,
 )
 
 from zoo_mcp import ZooMCPException, logger
@@ -142,6 +145,87 @@ async def text_to_cad_iteration(
         if response.code is None:
             return "Error: Text-to-CAD iteration response is null."
         return response.code
+    else:
+        if response.error is None:
+            return "Error: Text-to-CAD iteration response is null."
+        return response.error
+
+
+async def text_to_cad_multi_file_iteration(
+    prompt: str,
+    file_paths: list[str] | list[Path] | None = None,
+    proj_path: Path | str | None = None,
+) -> dict | str:
+    """Send a prompt and multiple KCL files to Zoo's Text-To-CAD multi-file iteration endpoint. Either file_paths or proj_path must be provided. If proj_path is provided all contained files will be sent to the endpoint.
+
+    Args:
+        prompt (str): a description of the changes to be made to the CAD model associated with the provided KCL files
+        file_paths (list[Path | str] | None): A list of paths to KCL files
+        proj_path (Path | str | None): A path to a directory containing a main.kcl file. All contained files will be sent to the endpoint.
+
+    Returns:
+        dict | str: A dictionary containing the complete KCL code of the CAD model if Text-To-CAD multi-file iteration was successful.
+                    Each key in the dict, refers to a kcl file path relative to the project path (determined by the commonpath if a project path is not supplied), and the value is the complete KCL code for that file.
+                    otherwise an error message from Text-To-CAD
+    """
+    logger.info("Sending KCL code prompt to Text-To-CAD multi file iteration")
+
+    # default to using the project if both are provided
+    if file_paths and proj_path:
+        logger.warning("Both file paths and project path provided, using project path")
+        file_paths = None
+
+    if file_paths:
+        file_paths = [Path(fp) for fp in file_paths]
+        # find the common path of all file paths to use as the project path
+        path_strings = [str(p.resolve()) for p in file_paths]
+        common_path = os.path.commonpath(path_strings)
+        proj_path = Path(common_path)
+    else:
+        logger.info("Finding all files in project path")
+        proj_path = Path(proj_path)
+        file_paths = list(proj_path.rglob("*"))
+
+    if not file_paths:
+        logger.error("No files paths provided or found in project path")
+        raise ZooMCPException("No file paths provided or found in project path")
+
+    if ".kcl" not in [fp.suffix for fp in file_paths]:
+        logger.error("No .kcl extension found in provided file paths")
+        raise ZooMCPException("No .kcl extension found in provided file paths")
+
+    file_attachments = {
+        str(fp.relative_to(proj_path)): fp for fp in file_paths if fp.is_file()
+    }
+
+    t2cmfi = kittycad_client.ml.create_text_to_cad_multi_file_iteration(
+        body=TextToCadMultiFileIterationBody(
+            source_ranges=[],
+            prompt=prompt,
+        ),
+        file_attachments=file_attachments,
+    )
+
+    # get the response based on the request id
+    result = kittycad_client.ml.get_text_to_cad_model_for_user(id=t2cmfi.id)
+
+    # check if the request has either completed or failed, otherwise sleep and try again
+    while result.root.status not in [ApiCallStatus.COMPLETED, ApiCallStatus.FAILED]:
+        result = kittycad_client.ml.get_text_to_cad_model_for_user(id=t2cmfi.id)
+        await asyncio.sleep(1)
+
+    # get the data object (root) of the response
+    response = result.root
+
+    # check the data type of the response
+    if not isinstance(response, OptionTextToCadMultiFileIteration):
+        return "Error: Text-to-CAD response is not of type OptionTextToCadMultiFileIteration."
+
+    # if Text To CAD iteration was successful return the KCL code, otherwise return the error
+    if response.status == ApiCallStatus.COMPLETED:
+        if response.outputs is None:
+            return "Error: Text-to-CAD iteration response is null."
+        return response.outputs
     else:
         if response.error is None:
             return "Error: Text-to-CAD iteration response is null."
