a37b15b7714c3851eaba43d263ab68ad50478a2d
Ryan Barton <ryan@zoo.dev>
2025-09-19 10:23:58 -0400

    remove the iteration endpoint and rename multi file iteration to edit_kcl_project

--- src/zoo_mcp/ai_tools.py
+++ src/zoo_mcp/ai_tools.py
@@ -1,19 +1,15 @@
 import asyncio
-import os
 from pathlib import Path
 
-import aiofiles
 from kittycad import KittyCAD
 from kittycad.models import (
     ApiCallStatus,
     FileExportFormat,
     TextToCadCreateBody,
-    TextToCadIterationBody,
     TextToCadMultiFileIterationBody,
 )
 from kittycad.models.text_to_cad_response import (
     OptionTextToCad,
-    OptionTextToCadIteration,
     OptionTextToCadMultiFileIteration,
 )
 
@@ -72,127 +68,39 @@ async def text_to_cad(prompt: str) -> str:
         return response.error
 
 
-async def text_to_cad_iteration(
+async def edit_kcl_project(
     prompt: str,
-    kcl_code: str | None = None,
-    kcl_path: Path | str | None = None,
-) -> str:
-    """Send a prompt to Zoo's Text-To-CAD iteration endpoint. Either kcl_code or kcl_path must be provided. If kcl_path is provided, it should point to a .kcl file
-
-    Args:
-        prompt (str): a description of the changes to be made to the CAD model
-        kcl_code (str | None): The existing KCL code to be modified
-        kcl_path (Path | str | None): KCL path, the path should point to a .kcl file
-
-    Returns:
-        A string containing the complete KCL code of the CAD model if Text-To-CAD iteration was successful, otherwise an error
-        message from Text-To-CAD
-    """
-
-    logger.info("Sending KCL code prompt to Text-To-CAD iteration")
-
-    # default to using the code if both are provided
-    if kcl_code and kcl_path:
-        logger.warning("Both code and kcl_path provided, using code")
-        kcl_path = None
-
-    if kcl_path:
-        kcl_path = Path(kcl_path)
-        if kcl_path.is_file() and kcl_path.suffix != ".kcl":
-            logger.error("The provided kcl_path is not a .kcl file")
-            raise ZooMCPException("The provided kcl_path is not a .kcl file")
-        if kcl_path.is_dir():
-            logger.error("The provided kcl_path is a directory not a .kcl file")
-            raise ZooMCPException(
-                "The provided kcl_path is a directory not a .kcl file"
-            )
-
-    if not kcl_code and not kcl_path:
-        logger.error("Neither code nor kcl_path provided")
-        raise ZooMCPException("Neither code nor kcl_path provided")
-
-    if kcl_path:
-        async with aiofiles.open(kcl_path, "r") as inp:
-            kcl_code = await inp.read()
-
-    if not isinstance(kcl_code, str):
-        logger.error("kcl_code is not a string")
-        raise ZooMCPException("kcl_code is not a string")
-
-    t2ci = kittycad_client.ml.create_text_to_cad_iteration(
-        body=TextToCadIterationBody(
-            original_source_code=kcl_code, prompt=prompt, source_ranges=[]
-        )
-    )
-
-    # get the response based on the request id
-    result = kittycad_client.ml.get_text_to_cad_model_for_user(id=t2ci.id)
-
-    # check if the request has either completed or failed, otherwise sleep and try again
-    while result.root.status not in [ApiCallStatus.COMPLETED, ApiCallStatus.FAILED]:
-        result = kittycad_client.ml.get_text_to_cad_model_for_user(id=t2ci.id)
-        await asyncio.sleep(1)
-
-    # get the data object (root) of the response
-    response = result.root
-
-    # check the data type of the response
-    if not isinstance(response, OptionTextToCadIteration):
-        return "Error: Text-to-CAD response is not of type OptionTextToCadIteration."
-
-    # if Text To CAD iteration was successful return the KCL code, otherwise return the error
-    if response.status == ApiCallStatus.COMPLETED:
-        if response.code is None:
-            return "Error: Text-to-CAD iteration response is null."
-        return response.code
-    else:
-        if response.error is None:
-            return "Error: Text-to-CAD iteration response is null."
-        return response.error
-
-
-async def text_to_cad_multi_file_iteration(
-    prompt: str,
-    file_paths: list[str] | list[Path] | None = None,
-    proj_path: Path | str | None = None,
+    proj_path: Path | str,
 ) -> dict | str:
-    """Send a prompt and multiple KCL files to Zoo's Text-To-CAD multi-file iteration endpoint. Either file_paths or proj_path must be provided. If proj_path is provided all contained files will be sent to the endpoint.
+    """Send a prompt and a project to Zoo's Text-To-CAD edit kcl project endpoint. The proj_path will upload all contained files will be sent to the endpoint. There must be a main.kcl file in the root of the project.
 
     Args:
-        prompt (str): a description of the changes to be made to the CAD model associated with the provided KCL files
-        file_paths (list[Path | str] | None): A list of paths to KCL files
-        proj_path (Path | str | None): A path to a directory containing a main.kcl file. All contained files will be sent to the endpoint.
+        prompt (str): a description of the changes to be made to the KCL project associated with the provided KCL files
+        proj_path (Path | str ): A path to a directory containing a main.kcl file. All contained files (found recursively) will be sent to the endpoint.
 
     Returns:
         dict | str: A dictionary containing the complete KCL code of the CAD model if Text-To-CAD multi-file iteration was successful.
                     Each key in the dict, refers to a kcl file path relative to the project path (determined by the commonpath if a project path is not supplied), and the value is the complete KCL code for that file.
                     otherwise an error message from Text-To-CAD
     """
-    logger.info("Sending KCL code prompt to Text-To-CAD multi file iteration")
+    logger.info("Sending KCL code prompt to Text-To-CAD edit kcl project")
 
-    # default to using the project if both are provided
-    if file_paths and proj_path:
-        logger.warning("Both file paths and project path provided, using project path")
-        file_paths = None
-
-    if file_paths:
-        file_paths = [Path(fp) for fp in file_paths]
-        # find the common path of all file paths to use as the project path
-        path_strings = [str(p.resolve()) for p in file_paths]
-        common_path = os.path.commonpath(path_strings)
-        proj_path = Path(common_path)
-    else:
-        logger.info("Finding all files in project path")
-        proj_path = Path(proj_path)
-        file_paths = list(proj_path.rglob("*"))
+    logger.info("Finding all files in project path")
+    proj_path = Path(proj_path)
+    file_paths = list(proj_path.rglob("*"))
+    logger.info("Found %s files in project path", len(file_paths))
 
     if not file_paths:
         logger.error("No files paths provided or found in project path")
         raise ZooMCPException("No file paths provided or found in project path")
 
     if ".kcl" not in [fp.suffix for fp in file_paths]:
-        logger.error("No .kcl extension found in provided file paths")
-        raise ZooMCPException("No .kcl extension found in provided file paths")
+        logger.error("No .kcl files found in the provided project path")
+        raise ZooMCPException("No .kcl files found in the provided project path")
+
+    if not (proj_path / "main.kcl").is_file():
+        logger.error("No main.kcl file found in the root of the provided project path")
+        raise ZooMCPException("No main.kcl file found in the root of the provided project path")
 
     file_attachments = {
         str(fp.relative_to(proj_path)): fp for fp in file_paths if fp.is_file()
